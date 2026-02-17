64dc82d4516b8aa659a7476875ce43304a05c0ca
mikinator <michaelmaduagwu630@gmail.com>
2026-02-17 21:54:56 +0100

    feat: implement repeat string exercise solution

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return "error"
+    }
+    for (let i = 0; i < num, i++) {
 
+    }
 };
 
 // Do not edit below this line
