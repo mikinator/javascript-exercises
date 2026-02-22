344ef983fd6a5f2f6813bc8baaf9666d225d1c3f
mikinator <michaelmaduagwu630@gmail.com>
2026-02-22 20:25:20 +0100

    feat: implement function for sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let start;
+    let end;
 
+    if (num1 < 0 || num2 < 0 || !Number.isInteger(num1) || !Number.isInteger(num2))
+        return "ERROR"
+    if (num2 < num1) {
+        start = num2;
+        end = num1;
+    }
+    else {
+        start = num1;
+        end = num2;
+    }
+
+    let termCount = end - start + 1;
+    return termCount * (start + end) / 2;
 };
 
 // Do not edit below this line
