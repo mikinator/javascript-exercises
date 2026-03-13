4b8758dd0e78e9726a74b1cc31d222cf3c9ad6ca
mikinator <michaelmaduagwu630@gmail.com>
2026-03-13 12:31:21 +0100

    feat: implement palindrome logic and remove .skip() to enable tests

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    word = [...word].filter(char => char.toLowerCase() != char.toUpperCase() 
+    || /[0-9]/.test(char)).join('').toLowerCase();
+    let wordLength = word.length - 1;
+    for (let i = 0; i < word.length; i++) {
+        if (word[i] != word[wordLength--]) return false;
+    }
+    return true;
 };
 
 // Do not edit below this line
