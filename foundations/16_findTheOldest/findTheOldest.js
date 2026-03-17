886f2957ce9d46a7af44d4eec89b7cef6a695423
mikinator <michaelmaduagwu630@gmail.com>
2026-03-18 00:09:19 +0100

    feat: implement function to find object with oldest person

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,22 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    people.map(person => {
+        if (person.yearOfDeath === undefined) {
+            person.yearOfDeath = new Date().getFullYear();
+        }
+    })
+    people.sort((a,b) => ((b.yearOfDeath - b.yearOfBirth) - (a.yearOfDeath - a.yearOfBirth)));
+    return people[0];
 };
 
 // Do not edit below this line
 module.exports = findTheOldest;
+
+/**
+ * FIND AGE
+ * GET yearofbirth - yearofdeath
+ * IF person[i].yearofdeath == undefined
+ *  SET person[i] TO new DATE.now()
+ *  
+ * 
+ * 
+ */
