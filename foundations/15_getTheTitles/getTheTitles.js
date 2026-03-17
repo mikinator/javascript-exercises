f71b734da52b4e65007733d6fbda69cee426d9ac
mikinator <michaelmaduagwu630@gmail.com>
2026-03-17 20:12:05 +0100

    feat: implement book extract function

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    const arr = [];
+    let i = 0;
+    for (let book of books) {
+      arr[i] = book.title;
+      i++;
+    }
+    return arr;
 };
 
 // Do not edit below this line
